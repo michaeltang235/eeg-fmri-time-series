d0c8f6bc22b64c57aa3c6a01313a64ca883e4b33
michaeltang235 <62654864+michaeltang235@users.noreply.github.com>
2021-04-01 18:08:14 -0600

    Add files via upload
    allowed user to specify path to spm package, so that tissue probability map will then be automatically located without manually stating its location, Also, used filesep instead of '\' or '/' when formatting paths of files, so the script can be run on different OS without requiring too many modifications on itself

--- preproc_anat.m
+++ preproc_anat.m
@@ -9,12 +9,19 @@ close all
 %---------------------------------------------------------------------------
 % BEGIN USER INPUT
 
-% enter directory and filename of input file(s)
-directname = '/Users/michaeltang/Downloads/fmri_project/sub41';
-filename = '3Danat.nii';   % name of anatomical 3d image
+% enter subject number (str, e.g. sub31, sub27, ...)
+subnum = '14';
+
+% enter path to directory where all input files are located
+directname = ['C:\Users\siumichael.tang\Downloads\fmri_project\', 'sub', subnum];
+% directname = ['/Users/michaeltang/Downloads/fmri_project/', 'sub', subnum, '_imthres0_exmask'];
+filename = '3danat.nii';   % name of anatomical 3d image
 
 % enter final voxel size in (mm)
 voxelsize = [1 1 1];
+
+% enter full path to spm package
+spm_path = 'C:\Users\siumichael.tang\Downloads\spm12';
 
 % END USER INPUT
 %---------------------------------------------------------------------------
@@ -24,19 +31,23 @@ voxelsize = [1 1 1];
 % get full path of image file
 filename_path = fullfile(directname, append(filename, ',1'));
 
-% initialize spm module without graphical interface
+% format path to tissue probability map (tpm image) stored in SPM package
+tpm_path = [spm_path, filesep, 'tpm', filesep, 'TPM.nii'];
+
+% % initialize spm module without graphical interface
 spm('defaults','fmri');
 spm_jobman('initcfg');
 
 % END Preliminary
 %---------------------------------------------------------------------------
+
 % FROM SPM:
 % Normalization: Estimate and write: only step required for anat. image
 matlabbatch{1}.spm.spatial.normalise.estwrite.subj.vol = {filename_path};
 matlabbatch{1}.spm.spatial.normalise.estwrite.subj.resample = {filename_path};
 matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.biasreg = 0.0001;
 matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.biasfwhm = 60;
-matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.tpm = {'/Users/michaeltang/Downloads/spm12/tpm/TPM.nii'};
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.tpm = tpm_path;
 matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.affreg = 'mni';
 matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.reg = [0 0.001 0.5 0.05 0.2];
 matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.fwhm = 0;
