c945cc9961d1f6f153bf23a3e1745731ca2c4f57
michaeltang235 <62654864+michaeltang235@users.noreply.github.com>
2023-10-02 16:31:38 -0600

    Add files via upload

---
+++ code/preproc_anat.m
@@ -0,0 +1,74 @@
+clear all 
+close all
+%---------------------------------------------------------------------------
+% This script normalizes 3d anatomical image into standardized (MNI) space,
+% using the preprocessing step of normalization provided by SPM
+
+% enter info. about the image file before running the script
+%---------------------------------------------------------------------------
+%---------------------------------------------------------------------------
+% BEGIN USER INPUT
+
+% enter subject number (str, e.g. sub31, sub27, ...)
+subnum = '14';
+
+% enter path to directory where all input files are located
+directname = ['/work/levan_lab/mtang/fmri_project/', 'sub', subnum]; 
+%directname = ['C:\Users\siumichael.tang\Downloads\fmri_project\', 'sub', subnum];
+% directname = ['/Users/michaeltang/Downloads/fmri_project/', 'sub', subnum, '_imthres0_exmask'];
+filename_format = '3*anat*.nii';   % name of anatomical 3d image
+
+% enter final voxel size in (mm)
+voxelsize = [1 1 1];
+
+% enter full path to spm package
+spm_path = '/home/siumichael.tang/spm12';
+%spm_path = 'C:\Users\siumichael.tang\Downloads\spm12';
+
+% END USER INPUT
+%---------------------------------------------------------------------------
+%---------------------------------------------------------------------------
+% Preliminary:
+
+% get full path of input anatomical image file using get_path
+[~, file_path, ~] = get_path(directname, filename_format);
+
+% append '1' at the end of file_path, denoting the first image to be read
+% by spm
+filename_path = [file_path{:}, ',1'];
+
+% format path to tissue probability map (tpm image) stored in SPM package
+tpm_path = [spm_path, filesep, 'tpm', filesep, 'TPM.nii'];
+
+% add path of spm to current directory
+addpath(spm_path);
+
+% % initialize spm module without graphical interface
+spm('defaults','fmri');
+spm_jobman('initcfg');
+
+% END Preliminary
+%---------------------------------------------------------------------------
+
+% FROM SPM:
+% Normalization: Estimate and write: only step required for anat. image
+matlabbatch{1}.spm.spatial.normalise.estwrite.subj.vol = {filename_path};
+matlabbatch{1}.spm.spatial.normalise.estwrite.subj.resample = {filename_path};
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.biasreg = 0.0001;
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.biasfwhm = 60;
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.tpm = {tpm_path};
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.affreg = 'mni';
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.reg = [0 0.001 0.5 0.05 0.2];
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.fwhm = 0;
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.samp = 3;
+matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.bb = [-78 -112 -70
+                                                             78 76 85];
+matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.vox = voxelsize;
+matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.interp = 4;
+matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.prefix = 'w';
+
+% end Normalization: Estimate and write:
+%---------------------------------------------------------------------------
+
+% call spm_jobman to run the script above
+spm_jobman('run', matlabbatch);
